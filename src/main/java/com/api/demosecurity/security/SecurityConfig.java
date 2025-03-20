2104da389b6fdc990208df6e5097805bb37af403
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-03-20 14:43:29 +0100

    add users and roles

--- src/main/java/com/api/demosecurity/security/SecurityConfig.java
+++ src/main/java/com/api/demosecurity/security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.api.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -14,7 +18,29 @@ public class SecurityConfig {
         UserDetails juan = User.builder()
                 .username("Juan")
                 .password("{noop}123")
+                .roles("PROFESOR")
                 .build();
-        return  new InMemoryUserDetailsManager(juan);
+
+        UserDetails pedro = User.builder()
+                .username("pedro")
+                .password("{noop}123")
+                .roles("DIRECTOR")
+                .build();
+        return  new InMemoryUserDetailsManager(juan, pedro);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/students").hasRole("PROFESOR")
+                        .requestMatchers(HttpMethod.GET, "/api/students/**").hasRole("PROFESOR")
+                        .requestMatchers(HttpMethod.POST, "/api/students").hasRole("DIRECTOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/students").hasRole("DIRECTOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/students/**").hasRole("DIRECTOR")
+        );
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
